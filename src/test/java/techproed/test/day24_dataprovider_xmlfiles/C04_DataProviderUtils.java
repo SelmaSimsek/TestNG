e4576615ca5c69e24a591c2c3ac1eb56ddf3fdf2
Selma Simsek <selmasimsek0406@gmail.com>
2023-12-03 23:57:52 +0100

    v5

---
+++ src/test/java/techproed/test/day24_dataprovider_xmlfiles/C04_DataProviderUtils.java
@@ -0,0 +1,30 @@
+package techproed.test.day24_dataprovider_xmlfiles;
+
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import techproed.pages.DataProviderPage;
+
+import techproed.utilities.Driver;
+
+public class C04_DataProviderUtils {
+    @Test(dataProvider = "positiveTestData", dataProviderClass = C04_DataProviderUtils.class)
+    public void test01(int age) {
+
+        //https://dataprovider.netlify.app/ adresine gidelim
+        Driver.getDriver().get("https://dataprovider.netlify.app/");
+
+
+        //BlackBoxTesting techniques ile otomasyon test yapalim
+        DataProviderPage dataProviderPage=new DataProviderPage();
+        dataProviderPage.searcBox.sendKeys(String.valueOf(age), Keys.TAB,Keys.ENTER);
+
+        Assert.assertTrue(dataProviderPage.positiveVerifyMessage.isDisplayed());
+        //Driveri kapatalim
+        Driver.closeDriver();
+
+
+
+
+    }
+}
